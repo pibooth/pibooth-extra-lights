bec5654206fb4144f731da5ab717cc1ad3ef8fb5
anxuae <anxuae-prog@yahoo.fr>
2023-05-14 13:14:44 +0200

    Add new options to choose pin active state

--- pibooth_extra_lights.py
+++ pibooth_extra_lights.py
@@ -14,10 +14,16 @@ def pibooth_configure(cfg):
     """Declare the new configuration options"""
     cfg.add_option('CONTROLS', 'startup_led_pin', 29,
                    "Physical GPIO OUT pin to light a LED at pibooth startup (list of pins accepted)")
+    cfg.add_option('CONTROLS', 'startup_led_active_high', True,
+                   "If True, LED is lighting by setting GPIO OUT pin(s) to HIGH else by setting them to LOW")
     cfg.add_option('CONTROLS', 'preview_led_pin', 31,
                    "Physical GPIO OUT pin to light a LED during the entire capture sequence (list of pins accepted)")
+    cfg.add_option('CONTROLS', 'preview_led_active_high', True,
+                   "If True, LED is lighting by setting GPIO OUT pin(s) to HIGH else by setting them to LOW")
     cfg.add_option('CONTROLS', 'flash_led_pin', 33,
                    "Physical GPIO OUT pin to light a LED when the capture is taken (list of pins accepted)")
+    cfg.add_option('CONTROLS', 'flash_led_active_high', True,
+                   "If True, LED is lighting by setting GPIO OUT pin(s) to HIGH else by setting them to LOW")
 
 
 @pibooth.hookimpl
@@ -28,13 +34,16 @@ def pibooth_startup(app, cfg):
                use BOARD pin numbering.
     """
     app.led_startup = LEDBoard(*("BOARD{}".format(pin)
-                               for pin in cfg.gettuple('CONTROLS', 'startup_led_pin', int)))
+                               for pin in cfg.gettuple('CONTROLS', 'startup_led_pin', int)),
+                               active_high=cfg.getboolean('CONTROLS', 'startup_led_active_high'))
 
     app.led_sequence = LEDBoard(*("BOARD{}".format(pin)
-                                for pin in cfg.gettuple('CONTROLS', 'preview_led_pin', int)))
+                                for pin in cfg.gettuple('CONTROLS', 'preview_led_pin', int)),
+                                active_high=cfg.getboolean('CONTROLS', 'preview_led_active_high'))
 
     app.led_flash = LEDBoard(*("BOARD{}".format(pin)
-                             for pin in cfg.gettuple('CONTROLS', 'flash_led_pin', int)))
+                             for pin in cfg.gettuple('CONTROLS', 'flash_led_pin', int)),
+                             active_high=cfg.getboolean('CONTROLS', 'flash_led_active_high'))
 
     app.led_startup.on()
 
